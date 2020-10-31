e8f065c952ec81c5851fa45df6680785c544c76e
ayoupov <ayoupov@gmail.com>
2020-11-01 00:49:53 +0300

    + read odh

--- src/main/java/org/panaggelica/xls_json_sieve/model/ODHModel.java
+++ src/main/java/org/panaggelica/xls_json_sieve/model/ODHModel.java
@@ -1,9 +1,21 @@
 package org.panaggelica.xls_json_sieve.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
+import org.locationtech.jts.geom.Geometry;
+import org.n52.jackson.datatype.jts.GeometryDeserializer;
+import org.n52.jackson.datatype.jts.GeometrySerializer;
 
 @Data
 public class ODHModel {
-
+    String name;
+//"root_id": 656513337, "distance": 146.1, "end_date": "3000-01-01", "geometry":
+    Long root_id;
+    double distance;
+    String end_date;
+    @JsonSerialize(using = GeometrySerializer.class)
+    @JsonDeserialize(contentUsing = GeometryDeserializer.class)
+    Geometry geometry;
 
 }
--- src/main/java/org/panaggelica/xls_json_sieve/util/IOUtil.java
+++ src/main/java/org/panaggelica/xls_json_sieve/util/IOUtil.java
@@ -1,5 +1,6 @@
 package org.panaggelica.xls_json_sieve.util;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.n52.jackson.datatype.jts.JtsModule;
 
@@ -9,5 +10,6 @@ public class IOUtil {
 
     static {
         objectMapper.registerModule(new JtsModule());
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     }
 }
